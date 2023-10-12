5f57e5c83cf1dd403eac6a47aabd45d8dc98e057
sandrewtx08 <sandrewtx08@protonmail.com>
2023-10-12 16:07:42 -0300

    chore: manifests

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,30 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 import { resolve } from "path";
+import { readFileSync } from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
+	plugins: [
+		preact(),
+		{
+			name: "browser-manifest",
+			buildStart() {
+				this.emitFile({
+					type: "asset",
+					fileName: "manifest.json",
+					source: readFileSync(
+						resolve(
+							__dirname,
+							"manifest",
+							`${String(process.env.BROWSER).toLocaleLowerCase()}.json`,
+						),
+						"utf8",
+					),
+				});
+			},
+		},
+	],
 	build: {
 		rollupOptions: {
 			input: {
